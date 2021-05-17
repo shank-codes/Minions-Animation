0dd7b52455774d206a947d1d885b6dab001918d2
shank-codes <shashankhiremat22@gmail.com>
2021-05-17 18:54:36 +0530

    added left and right movement

--- main.cpp
+++ main.cpp
@@ -1,13 +1,14 @@
 #include<windows.h>
 #include<GL/glut.h>
 #include<stdlib.h>
+#include<iostream>
 #include<math.h>
 
 using namespace std;
 
-float angle[]={0,0,0,0,0},j,k=0;
+float angle[]={0,0,0,0,0},j,k=0,l=0,m;
 int turn[]={0,1,0,0},i,act=0,eyem=0,f=0;
-//int mood=0;
+int moveL=0,moveR=0;
 static int submenu_id;
 static int submenu_id1;
 
@@ -56,6 +57,18 @@ void Idle()
                 angle[4]+=0.5;
         else
             angle[4]=0;
+    }
+
+    //move left
+    if(moveL==1)
+    {
+        if(l<=501 && l>-500)
+            l-=0.3;
+    }
+    if(moveR==1)
+    {
+        if(l>=-501 && l<=500)
+            l+=0.3;
     }
 }
 void myReshape(int w,int h)
@@ -118,30 +131,56 @@ void land()
 }
 void body(int mood)
 {
-    if(mood==0)
-        glColor3f(1,1,0);
-    if(mood==1)
+    if(mood==1 &&( moveL==1 || moveR==1))
+    {
         glColor3f(0.9,0,0.9);
-    if(mood==2)
-        glColor3f(1,0.3,0.3);
+        glPushMatrix();
+        glTranslatef(l,200,0);   //setting base of a semicircle as y=200
+        circle(170,0,180);
+        glPopMatrix();
 
-    glPushMatrix();
-    glTranslatef(0,200,0);   //setting base of a semicircle as y=200
-    circle(170,0,180);
-    glPopMatrix();
+        glBegin(GL_POLYGON);
+        glVertex2f(-170+l,-50);
+        glVertex2f(170+l,-50);
+        glVertex2f(170+l,200);
+        glVertex2f(-170+l,200);
+        glEnd();
 
-    glBegin(GL_POLYGON);
-    glVertex2f(-170,-50);
-    glVertex2f(170,-50);
-    glVertex2f(170,200);
-    glVertex2f(-170,200);
-    glEnd();
+        glPushMatrix();
+        glTranslatef(l,-50,0);   //setting base of a semicircle as y=-50
+        glScalef(1,0.6,1);
+        circle(170,180,360);     // semicircle from 180 to 360
+        glPopMatrix();
 
-    glPushMatrix();
-    glTranslatef(0,-50,0);   //setting base of a semicircle as y=-50
-    glScalef(1,0.6,1);
-    circle(170,180,360);     // semicircle from 180 to 360
-    glPopMatrix();
+    }
+    else
+    {
+
+        if(mood==0)
+            glColor3f(1,1,0);
+        if(mood==1)
+            glColor3f(0.9,0,0.9);
+        if(mood==2)
+            glColor3f(1,0.3,0.3);
+
+        glPushMatrix();
+        glTranslatef(0,200,0);   //setting base of a semicircle as y=200
+        circle(170,0,180);
+        glPopMatrix();
+
+        glBegin(GL_POLYGON);
+        glVertex2f(-170,-50);
+        glVertex2f(170,-50);
+        glVertex2f(170,200);
+        glVertex2f(-170,200);
+        glEnd();
+
+        glPushMatrix();
+        glTranslatef(0,-50,0);   //setting base of a semicircle as y=-50
+        glScalef(1,0.6,1);
+        circle(170,180,360);     // semicircle from 180 to 360
+        glPopMatrix();
+    }
 
     /*
     glPushMatrix();
@@ -163,8 +202,45 @@ void body(int mood)
     glPopMatrix();
     */
 }
-void hair()
+void hair(int mood)
 {
+    if(mood==1 &&( moveL==1 || moveR==1))
+    {
+    glPushMatrix();
+    glTranslatef(-20+l,265,0);
+    glBegin(GL_LINE_STRIP);
+    glColor3f(0,0,0);
+    circle2(100,85,120);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    circle2(105,85,125);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    circle2(107,85,130);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    circle2(108,85,135);
+    glEnd();
+    glPopMatrix();
+
+    glPushMatrix();
+    glTranslatef(20+l,265,0);
+    glBegin(GL_LINE_STRIP);
+    circle3(100,95,60);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    circle3(105,95,55);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    circle3(107,95,50);
+    glEnd();
+    glBegin(GL_LINE_STRIP);
+    circle3(108,95,45);
+    glEnd();
+    glPopMatrix();
+    return;
+    }
+
     glPushMatrix();
     glTranslatef(-20,265,0);
     glBegin(GL_LINE_STRIP);
@@ -198,8 +274,31 @@ void hair()
     glEnd();
     glPopMatrix();
 }
-void specsframe()
+void specsframe(int mood)
 {
+    if(mood==1 &&( moveL==1 || moveR==1))
+    {
+        //LEFT
+    glColor3f(0,0,0);
+    glPushMatrix();
+    glTranslatef(-59+l,200,0);
+    glBegin(GL_POLYGON);
+    circle2(67,140,200);
+    glVertex2f(-110,0);
+    glVertex2f(-110,30);
+    glEnd();
+    glPopMatrix();
+    //RIGHT
+    glPushMatrix();
+    glTranslatef(59+l,200,0);
+    glBegin(GL_POLYGON);
+    circle2(67,-20,40);
+    glVertex2f(110,30);
+    glVertex2f(110,0);
+    glEnd();
+    glPopMatrix();
+        return;
+    }
     //LEFT
     glColor3f(0,0,0);
     glPushMatrix();
@@ -267,6 +366,30 @@ void mouthangry()
 }
 void mouthzombie()
 {
+    if(moveL==1 || moveR==1)
+    {
+        glColor3f(0,0,0);
+        glPushMatrix();
+        glTranslatef(l,120,0);
+        glScalef(1,0.5,1);
+        glBegin(GL_LINE_STRIP);
+        glVertex2f(-80,-60);
+        glVertex2f(80,-60);
+        glEnd();
+        glColor3f(1,1,1);
+        glBegin(GL_TRIANGLES);
+        glVertex2f(-80,-60);
+        glVertex2f(-60,0);
+        glVertex2f(-40,-60);
+        glEnd();
+        glBegin(GL_TRIANGLES);
+        glVertex2f(80,-60);
+        glVertex2f(60,0);
+        glVertex2f(40,-60);
+        glEnd();
+        glPopMatrix();
+        return;
+    }
     glColor3f(0,0,0);
     glPushMatrix();
     glTranslatef(0,120,0);
@@ -290,6 +413,128 @@ void mouthzombie()
 }
 void hand(int mood)
 {
+    if(mood==1 &&( moveL==1 || moveR==1))
+    {
+        //RIGHT
+        if(mood==0)
+            glColor3f(1,1,0);
+        if(mood==1)
+            glColor3f(0.9,0,0.9);
+        if(mood==2)
+            glColor3f(1,0.3,0.3);
+
+        glPushMatrix();
+        glTranslatef(30+l,160,0);    //complete circle near eyes
+        circle(25,0,360);
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(165,28,0);
+        if(act==1)
+        {
+            glRotatef(angle[2],0,0,1);
+            glRotatef(40,0,0,1);
+        }
+        if(act==2)
+        glRotatef(angle[0],0,0,1);
+        if(act==0)
+            glRotatef(angle[0],0,0,1);    //angle =0, z=1
+
+        glBegin(GL_POLYGON);               //right hand rectangle
+        glVertex2f(0+l,-10);
+        glVertex2f(100+l,-10);
+        glVertex2f(100+l,10);
+        glVertex2f(0+l,10);
+        glEnd();
+
+        //gloves
+        glPushMatrix();
+        glTranslatef(125+l,0,0);
+        glRotatef(90,0,0,1);
+        glColor3f(0,0,0);
+        glPushMatrix();
+        glScalef(1,1.2,1);
+        glTranslatef(-15,0,0);
+        circle(12,0,360);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0,-6,0);
+        circle(12,0,360);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(12,0,0);
+        circle(12,0,360);
+        glPopMatrix();
+        glBegin(GL_POLYGON);
+        glVertex2f(-20,0);
+        glVertex2f(17,0);
+        glVertex2f(12,25);
+        glVertex2f(-17,25);
+        glEnd();
+        glPopMatrix();
+        glPopMatrix();
+
+        //LEFT
+        glPushMatrix();
+        glTranslatef(-160+l,30,0);
+        if(mood==0)
+            glColor3f(1,1,0);
+        if(mood==1)
+            glColor3f(0.9,0,0.9);
+        if(mood==2)
+            glColor3f(1,0.3,0.3);
+        circle(25,0,360);
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(-165+l,28,0);
+        if(act==2)
+            glRotatef(angle[2],0,0,1);
+        if(act==0)
+            glRotatef(-60,0,0,1);
+        if(act==1)
+            glRotatef(40,0,0,1);
+
+        glBegin(GL_POLYGON);
+        glVertex2f(0,-10);
+        glVertex2f(-100,-10);
+        glVertex2f(-100,10);
+        glVertex2f(0,10);
+        glEnd();
+
+        //gloves
+        glPushMatrix();
+        glTranslatef(-125,0,0);
+        glRotatef(-90,0,0,1);
+        glColor3f(0,0,0);
+
+        glPushMatrix();
+        glScalef(1,1.2,1);
+        glTranslatef(-15,0,0);
+        circle(12,0,360);
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(0,-6,0);
+        circle(12,0,360);
+        glPopMatrix();
+
+        glPushMatrix();
+        glTranslatef(12,0,0);
+        circle(12,0,360);
+        glPopMatrix();
+
+        glBegin(GL_POLYGON);
+        glVertex2f(-20,0);
+        glVertex2f(17,0);
+        glVertex2f(12,25);
+        glVertex2f(-17,25);
+        glEnd();
+        glPopMatrix();
+
+        glPopMatrix();
+            return;
+    }
     //RIGHT
     if(mood==0)
         glColor3f(1,1,0);
@@ -604,8 +849,8 @@ void display()
     if(act==3)
         glRotatef(angle[3],0,0,1);
     body(mood);
-    hair();
-    specsframe();
+    hair(mood);
+    specsframe(mood);
 
     if(mood==0)
         mouthhappy();
@@ -616,14 +861,11 @@ void display()
     hand(mood);
     cloth();
     eyes(mood);
-
-    if(act==3)
-       glPopMatrix();
     legsi(mood);
     legs();
-    if(act!=3)
-        glPopMatrix();
+    glPopMatrix();
     }
+
     for(int i=-1000;i<=1000;i+=1000)
     {
         glPushMatrix();
@@ -631,6 +873,7 @@ void display()
         shoes();
         glPopMatrix();
     }
+
     glFlush();
     glutPostRedisplay();
     glutSwapBuffers();     //glutSwapBuffers swaps the buffers of the current window if double buffered
@@ -639,9 +882,13 @@ void flag_menu(int num)
 {
     switch(num)
     {
-        case 1://mood=0;
+        case 1:
+            moveL=1;
+            moveR=0;
                 break;
-        case 2://mood=1;
+        case 2:
+            moveL=0;
+            moveR=1;
                 break;
         case 4:act=1;
                 break;
@@ -651,7 +898,10 @@ void flag_menu(int num)
                 break;
         case 8:eyem=0;
                 break;
-        case 9:act=0;
+        case 9:
+                act=0;
+                moveL=0;
+                moveR=0;
                 eyem=0;
                 break;
         case 10:exit(0);
